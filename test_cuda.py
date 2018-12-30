4b9cf701bab0f98014a1b91c6a6a073c8927a52e
stanleyjacob <stanleyjacob@users.noreply.github.com>
2018-12-30 01:05:42 -0800

    Create test_cuda.py

---
+++ test_cuda.py
@@ -0,0 +1,31 @@
+import pycuda.compiler as comp
+import pycuda.driver as drv
+import numpy
+import pycuda.autoinit
+import time
+
+mod = comp.SourceModule("""
+__global__ void multiply_them(float *dest, float *a, float *b)
+{
+  const int i = threadIdx.x;
+  dest[i] = a[i] * b[i];
+}
+""")
+
+multiply_them = mod.get_function("multiply_them")
+
+a = numpy.random.randn(400).astype(numpy.float32)
+b = numpy.random.randn(400).astype(numpy.float32)
+
+start = time.clock()
+dest = numpy.zeros_like(a)
+multiply_them(
+        drv.Out(dest), drv.In(a), drv.In(b),
+        block=(400,1,1))
+end = time.clock()
+print(end - start)
+
+start = time.clock()
+c = a*b
+end = time.clock()
+print(end - start)
